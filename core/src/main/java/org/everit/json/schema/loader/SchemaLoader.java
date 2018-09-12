5814524b067ba7c4e137edddf6ba122c1f7c4546
Bence Eros <ebence88@gmail.com>
2018-09-12 15:11:10 +0200

    sketching up some ideas re. custom keyword support; moving AbstractSchemaExtractor to separate file

--- core/src/main/java/org/everit/json/schema/loader/SchemaLoader.java
+++ core/src/main/java/org/everit/json/schema/loader/SchemaLoader.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Function;
 
 import org.everit.json.schema.CombinedSchema;
 import org.everit.json.schema.EmptySchema;
@@ -224,6 +225,10 @@ public class SchemaLoader {
 
         public SchemaLoaderBuilder regexpFactory(RegexpFactory regexpFactory) {
             this.regexpFactory = regexpFactory;
+            return this;
+        }
+
+        public <E extends SchemaExtractor> SchemaLoaderBuilder registerSchemaExtractor(Function<SchemaLoader, E> extractorFactory) {
             return this;
         }
     }
